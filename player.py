15c870298196e288b65fc919e4f31af1cc32eefc
stevenpfaff <pfaffy1426@gmail.com>
2021-09-13 10:31:06 -0500

    computer random generator

--- player.py
+++ player.py
@@ -1,7 +1,14 @@
+from random import choice
+
+
 class Player:
     def __init__(self,name):
         self.name = " "
+        self.score = 0
+        self.choice = None
 
-          def gesture_input(self):
-            input("Choose 'rock', 'paper', 'scissors', 'lizard', or 'spock'")
-        
+          def choose(self):
+            self.choice = int(raw_input("Rock [1] Paper [2] Scissors [3] Lizard [4] Spock [5]"))
+            return self.choice
+            
+    
