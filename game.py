421db14edd87c93dac01187186be4f0470652f95
stevenpfaff <pfaffy1426@gmail.com>
2021-09-14 16:48:59 -0500

    fixed ai game

--- game.py
+++ game.py
@@ -21,7 +21,7 @@ class Game:
         self.player_vs_ai()
         self.player_vs_human()
            
-    
+
     def player_choice(self):
         self.ai_game = Game()
         self.human_game = Game()
@@ -36,36 +36,36 @@ class Game:
         self.ai = Computer()
         self.player1.get_selection()
         self.ai.get_selection()
-        if self.player1.choice == "rock" and self.ai.choice == "lizard" or "scissors":
+        if self.player1.choice == "rock" and (self.ai.choice == "lizard" or self.ai.choice == "scissors"):
             print("Player 1 Wins!")
             self.player1.score += 1
-        elif self.player1.choice == "scissors" and self.ai.choice == "lizard" or "paper":
+        elif self.player1.choice == "scissors" and (self.ai.choice == "lizard" or self.ai.choice == "paper"):
             print("Player 1 Wins!")
             self.player1.score += 1
-        elif self.player1.choice == "paper" and self.ai.choice == "rock" or "spock":
+        elif self.player1.choice == "paper" and (self.ai.choice == "rock" or self.ai.choice == "spock"):
             print("Player 1 Wins!")
             self.player1.score += 1
-        elif self.player1.choice == "spock" and self.ai.choice == "scissors" or "rock":
+        elif self.player1.choice == "spock" and (self.ai.choice == "scissors" or self.ai.choice == "rock"):
             print("Player 1 Wins!")
             self.player1.score += 1
-        elif self.player1.choice == "lizard" and self.ai.choice == "spock" or "paper":
+        elif self.player1.choice == "lizard" and (self.ai.choice == "spock" or self.ai.choice == "paper"):
             print("Player 1 Wins!")
             self.player1.score += 1   
         elif self.player1.choice == self.ai.choice:
             print("Its a tie!")
-        elif self.ai.choice == "rock" and self.player1.choice == "lizard" or "scissors":
+        elif self.ai.choice == "rock" and (self.player1.choice == "lizard" or self.player1.choice ==  "scissors"):
             print("Computer Wins!")
             self.ai.score += 1
-        elif self.ai.choice == "scissors" and self.player1.choice == "lizard" or "paper":
+        elif self.ai.choice == "scissors" and (self.player1.choice == "lizard" or self.player1.choice == "paper"):
             print("Computer Wins!")
             self.ai.score += 1
-        elif self.ai.choice == "paper" and self.player1.choice == "rock" or "spock":
+        elif self.ai.choice == "paper" and (self.player1.choice == "rock" or self.player1.choice == "spock"):
             print("Computer Wins!")
             self.ai.score += 1
-        elif self.ai.choice == "spock" and self.player1.choice == "scissors" or "rock":
+        elif self.ai.choice == "spock" and (self.player1.choice == "scissors" or self.player1.choice == "rock"):
             print("Computer Wins!")
             self.ai.score += 1
-        elif self.ai.choice == "lizard" and self.player1.choice == "spock" or "paper":
+        elif self.ai.choice == "lizard" and (self.player1.choice == "spock" or self.player1.choice == "paper"):
             print("Computer Wins!")
             self.ai.score += 1
         elif self.player1.choice != "rock" "paper" "scissors" "lizard" or "spock":
@@ -76,48 +76,11 @@ class Game:
         self.player2 = Human()
         self.player1.get_selection()
         self.player2.get_selection()
-        if self.player1.choice == "rock" and self.player2.choice == "lizard" or "scissors":
-            print("Player 1 Wins!")
-            self.player1.score += 1
-        elif self.player1.choice == "scissors" and self.player2.choice == "lizard" or "paper":
-            print("Player 1 Wins!")
-            self.player1.score += 1
-        elif self.player1.choice == "paper" and self.player2.choice== "rock" or "spock":
-            print("Player 1 Wins!")
-            self.player1.score += 1
-        elif self.player1.choice == "spock" and self.player2.choice == "scissors" or "rock":
-            print("Player 1 Wins!")
-            self.player1.score += 1
-        elif self.player1.choice == "lizard" and self.player2.choice == "spock" or "paper":
-            print("Player 1 Wins!")
-            self.player1.score += 1   
-        elif self.player1.choice == self.player2.choice:
-            print("Its a tie!")
-        elif self.player2.choice == "rock" and self.player1.choice == "lizard" or "scissors":
-            print("Computer Wins!")
-            self.player2.score += 1
-        elif self.player2.choice == "scissors" and self.player1.choice == "lizard" or "paper":
-            print("Computer Wins!")
-            self.player2.score += 1
-        elif self.ai.choice == "paper" and self.player1.choice == "rock" or "spock":
-            print("Computer Wins!")
-            self.player2.score += 1
-        elif self.player2.choice == "spock" and self.player1.choice == "scissors" or "rock":
-            print("Computer Wins!")
-            self.player2.score += 1
-        elif self.player2.choice == "lizard" and self.player1.choice == "spock" or "paper":
-            print("Computer Wins!")
-            self.player2.score += 1
-        elif self.player1.choice != "rock" "paper" "scissors" "lizard" or "spock":
-            print("Please enter valid input!")
 
     def determine_winner(self):
-        self.player_score = Player()
-        if self.player.score == 2:
+        if Player.score == 2:
             print("You are the winner")
-        self.human_score = Human()
-        if self.human_score.score == 2:
+        if Human.score == 2:
             print("You are the winner")
-        self.computer_score = Computer()
-        if self.computer_score.score == 2:
+        if Computer.score == 2:
             print ("You are the winner") 
